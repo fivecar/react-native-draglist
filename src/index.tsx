fivecar/react-native-draglist

commit d73d4b73cea0d8adbf22efe5c38756e27a06dc3f

fix: #76 Sync ref state synchronously with render (#85)
* fix: #76 Sync ref state synchronously with render

- Eliminate `pan.setValue`, which stopped working reliably in recent RN
  versions if you're using native driver. Instead, always use Animation
  to set it.
- Move ref updates to `useMemo` in order to make them synchronous with
  render.

* Everything works on both platforms with this change.
- get rid of panOpacity
- abstract keyExtractor to change keys during reordering

* comment updates and simplifying of cell rendering back to memoized style

* update dataRef comments and optimize its use

* move activeKey and activeIndex to ActiveData. everything works.

* rename activeKey and activeIndex to key and index in ActiveData

* reinstate comment

src/index.tsx
@@ -1,7 +1,6 @@
 import React, {
   useCallback,
   useEffect,
-  useLayoutEffect,
   useMemo,
   useRef,
   useState,
@@ -16,12 +15,12 @@ import {
   NativeScrollEvent,
   NativeSyntheticEvent,
   PanResponder,
-  Platform,
   StyleProp,
   View,
   ViewStyle,
 } from "react-native";
 import {
+  ActiveData,
   DragListProvider,
   LayoutCache,
   PosExtent,
@@ -57,8 +56,6 @@ export interface DragListRenderItemInfo<T> extends ListRenderItemInfo<T> {
    */
   isActive: boolean;
 }
-
-const isAndroid = Platform.OS === "android";
 
 // Used merely to trigger FlatList to re-render when necessary. Changing the
 // activeKey or the panIndex should both trigger re-render.
@@ -98,51 +95,94 @@ function DragListImpl<T>(
     ...rest
   } = props;
   // activeKey and activeIndex track the item being dragged
-  const activeKey = useRef<string | null>(null);
-  const activeIndex = useRef(-1);
-  const reorderingRef = useRef(false);
+  const activeDataRef = useRef<ActiveData | null>(null);
+  const isReorderingRef = useRef(false); // Whether we're actively rendering a reorder right now.
   // panIndex tracks the location where the dragged item would go if dropped
   const panIndex = useRef(-1);
-  const panOpacity = useRef(new Animated.Value(1)).current;
   const [extra, setExtra] = useState<ExtraData>({
-    activeKey: activeKey.current,
+    activeKey: activeDataRef.current?.key ?? null,
     panIndex: -1,
   });
   const layouts = useRef<LayoutCache>({}).current;
-  const dataRef = useRef(data);
   const panGrantedRef = useRef(false);
   const grantScrollPosRef = useRef(0); // Scroll pos when granted
   // The amount you need to add to the touched position to get to the active
   // item's center.
   const grantActiveCenterOffsetRef = useRef(0);
-  const flatWrapRefPosUpdatedRef = useRef(false);
   const autoScrollTimerRef = useRef<ReturnType<typeof setInterval> | null>(
     null
   );
+
+  // The following refs exist only to avoid unnecessary re-renders, so we keep them up to date
+  // immediately using `useMemo` as opposed to `useEffect`, which allows them technically to be
+  // wrong/off for one render (since effects run after a render is done).
   const hoverRef = useRef(props.onHoverChanged);
+  // #78 - keep onHoverChanged up to date in our ref
+  hoverRef.current = useMemo(
+    () => props.onHoverChanged,
+    [props.onHoverChanged]
+  );
   const reorderRef = useRef(props.onReordered);
+  reorderRef.current = useMemo(() => props.onReordered, [props.onReordered]);
+
+  // #76 When we finalize a reordering (i.e. when our parent gets `onReordered`), we need to
+  // insulate ourselves from the parent changing the data we render without us controlling the
+  // syncing of that change with all our animation state. So we render from dataRef instead of data
+  // directly, so that during reordering, we don't see the parent's data change.
+  const dataRef = useRef(data);
+
   const flatRef = useRef<FlatList<T> | null>(null);
   const flatWrapRef = useRef<View>(null);
   const flatWrapLayout = useRef<PosExtent>({
     pos: 0,
     extent: 1,
   });
+  const flatWrapRefPosUpdatedRef = useRef(false);
   const scrollPos = useRef(0);
+
   // pan is the drag dy
   const pan = useRef(new Animated.Value(0)).current;
+  const setPan = useCallback(
+    (value: number) => {
+      // Starting RN 0.76.3, pan.setValue(whatever) no longer animates the isActive item. Dunno whether
+      // it's the useNativeDriver or what that gets this working again. So, lamely, we set the value
+      // using a zero-duration Animated.timing.
+      Animated.timing(pan, {
+        duration: 0,
+        toValue: value,
+        useNativeDriver: true,
+      }).start();
+    },
+    [pan]
+  );
+
+  // #76 - Add indirection to keyExtractor to change keys during reordering. Because native
+  // implementations can/do recycle views at times, you need to deliberately rekey things during
+  // reordering so the native implementation respects your new positions/styles/settings.
+  const keyExtractorRef = useRef(keyExtractor);
+  keyExtractorRef.current = useMemo(() => {
+    return (item: T, index: number) => {
+      const key = keyExtractor(item, index);
+      if (isReorderingRef.current) {
+        return key + "_reordering";
+      }
+      return key;
+    };
+  }, [keyExtractor]);
+
+  const shouldCapturePan = useCallback(() => {
+    return !!activeDataRef.current && !isReorderingRef.current;
+  }, []);
+
   const panResponder = useRef(
     PanResponder.create({
-      onStartShouldSetPanResponderCapture: () =>
-        !!activeKey.current && !reorderingRef.current,
-      onStartShouldSetPanResponder: () =>
-        !!activeKey.current && !reorderingRef.current,
-      onMoveShouldSetPanResponder: () =>
-        !!activeKey.current && !reorderingRef.current,
-      onMoveShouldSetPanResponderCapture: () =>
-        !!activeKey.current && !reorderingRef.current,
+      onStartShouldSetPanResponderCapture: shouldCapturePan,
+      onStartShouldSetPanResponder: shouldCapturePan,
+      onMoveShouldSetPanResponder: shouldCapturePan,
+      onMoveShouldSetPanResponderCapture: shouldCapturePan,
       onPanResponderGrant: (_, gestate) => {
         grantScrollPosRef.current = scrollPos.current;
-        pan.setValue(0);
+        setPan(0);
         panGrantedRef.current = true;
         flatWrapRefPosUpdatedRef.current = false;
         flatWrapRef.current?.measure(
@@ -158,10 +198,10 @@ function DragListImpl<T>(
               pos: props.horizontal ? pageX : pageY,
             };
             if (
-              activeKey.current &&
-              layouts.hasOwnProperty(activeKey.current)
+              activeDataRef.current &&
+              layouts.hasOwnProperty(activeDataRef.current.key)
             ) {
-              const itemLayout = layouts[activeKey.current];
+              const itemLayout = layouts[activeDataRef.current.key];
               const screenPos = props.horizontal ? gestate.x0 : gestate.y0;
               const clientViewPos = screenPos - flatWrapLayout.current.pos;
               const clientPos = clientViewPos + scrollPos.current;
@@ -187,8 +227,8 @@ function DragListImpl<T>(
 
         if (
           !flatWrapRefPosUpdatedRef.current ||
-          !activeKey.current ||
-          !layouts.hasOwnProperty(activeKey.current)
+          !activeDataRef.current ||
+          !layouts.hasOwnProperty(activeDataRef.current.key)
         ) {
           return;
         }
@@ -202,17 +242,7 @@ function DragListImpl<T>(
           const panAmount =
             scrollPos.current - grantScrollPosRef.current + movedAmount;
 
-          // https://github.com/fivecar/react-native-draglist/issues/53
-          // Starting RN 0.76.3, pan.setValue(whatever) no longer animates the
-          // isActive item. Dunno whether it's the useNativeDriver or what that
-          // gets this working again. So, lamely, we set the value using a
-          // zero-duration Animated.timing.
-          Animated.timing(pan, {
-            duration: 0,
-            easing: Easing.inOut(Easing.linear),
-            toValue: panAmount,
-            useNativeDriver: true,
-          }).start();
+          setPan(panAmount);
 
           // Now we figure out what your panIndex should be based on everyone's
           // heights, starting from the first element. Note that we can't do
@@ -224,7 +254,10 @@ function DragListImpl<T>(
           while (
             curIndex < dataRef.current.length &&
             layouts.hasOwnProperty(
-              (key = keyExtractor(dataRef.current[curIndex], curIndex))
+              (key = keyExtractorRef.current(
+                dataRef.current[curIndex],
+                curIndex
+              ))
             ) &&
             layouts[key].pos + layouts[key].extent <
               clientPos + grantActiveCenterOffsetRef.current
@@ -240,7 +273,7 @@ function DragListImpl<T>(
           }
         }
 
-        const dragItemExtent = layouts[activeKey.current].extent;
+        const dragItemExtent = layouts[activeDataRef.current.key].extent;
         const leadingEdge = wrapPos - dragItemExtent / 2;
         const trailingEdge = wrapPos + dragItemExtent / 2;
         let offset = 0;
@@ -273,17 +306,20 @@ function DragListImpl<T>(
         }
       },
       onPanResponderRelease: async (_, _gestate) => {
+        const activeIndex = activeDataRef.current?.index;
+
         if (autoScrollTimerRef.current) {
           clearInterval(autoScrollTimerRef.current);
           autoScrollTimerRef.current = null;
         }
         onDragEnd?.();
         if (
-          activeIndex.current !== panIndex.current &&
+          activeIndex != null && // Being paranoid, we exclude both undefined and null here
+          activeIndex !== panIndex.current &&
           // Ignore the case where you drag the last item beyond the end
           !(
-            activeIndex.current === dataRef.current.length - 1 &&
-            panIndex.current > activeIndex.current
+            activeIndex === dataRef.current.length - 1 &&
+            panIndex.current > activeIndex
           )
         ) {
           try {
@@ -293,10 +329,23 @@ function DragListImpl<T>(
             // quickly (e.g. if onReordered deletes an item, the next
             // onReordered call would be made on a list whose indices are
             // stale).
-            reorderingRef.current = true;
-            await reorderRef.current?.(activeIndex.current, panIndex.current);
+            isReorderingRef.current = true;
+
+            // #76 We need to control what we render so it's always in sync with our animation
+            // state. When we call onReordered, the parent can change the data we render without us
+            // being able to sync that change with our own state, so we insulate ourselves during
+            // this render by keeping our own copy of data. Our `useEffect` will run after the
+            // render that onReordered triggers, which will then restore our ref back to pointing at
+            // the parent's data.
+            const dataCopy = [...dataRef.current];
+            const itemToMove = dataCopy.splice(activeIndex, 1);
+            dataCopy.splice(panIndex.current, 0, itemToMove[0]);
+            dataRef.current = dataCopy;
+
+            await reorderRef.current?.(activeIndex, panIndex.current);
           } finally {
-            reorderingRef.current = false;
+            reset(); // Guarantee resetting by putting this in finally
+            isReorderingRef.current = false;
           }
         } else {
           // #76 - Only reset here if we're not going to reorder the list. If we are instead
@@ -309,11 +358,10 @@ function DragListImpl<T>(
   ).current;
 
   const reset = useCallback(() => {
-    activeIndex.current = -1;
-    activeKey.current = null;
+    activeDataRef.current = null;
     panIndex.current = -1;
     setExtra({ activeKey: null, panIndex: -1 });
-    pan.setValue(0);
+    setPan(0);
     panGrantedRef.current = false;
     grantActiveCenterOffsetRef.current = 0;
     if (autoScrollTimerRef.current) {
@@ -323,45 +371,21 @@ function DragListImpl<T>(
   }, []);
 
   useEffect(() => {
+    // #76 Deliberately sync dataRef with a useEffect, not a useMemo, so that we update it after
+    // rendering. This only truly matters during a reorder-triggered rendering, where we keep our
+    // own copy of `data`.
     dataRef.current = data;
   }, [data]);
 
-  useLayoutEffect(() => {
-    // #76 - Right before we render a reordered list, we hide the item being dragged. If we don't,
-    // an upcoming render will show it jumped back to its old spot briefly before it gets rendered
-    // back into its new spot (because the reset sets the pan position back to 0).
-    // #81 - Have to not do this on Android, because those items disappear forever. It's like
-    // Android recycles views and ignores the subsequent opacity reset, seemingly.
-    if (!isAndroid) {
-      panOpacity.setValue(0);
-    }
-    reset();
-
-    if (!isAndroid) {
-      // Lame, I know. Just need a way to reset opacity after the render is done.
-      setTimeout(() => panOpacity.setValue(1), 0);
-    }
-  }, [data, reset]);
-
-  // #78 - keep onHoverChanged up to date in our ref
-  useEffect(() => {
-    hoverRef.current = props.onHoverChanged;
-  }, [props.onHoverChanged]);
-
-  useEffect(() => {
-    reorderRef.current = props.onReordered;
-  }, [props.onReordered]);
-
   const renderDragItem = useCallback(
     (info: ListRenderItemInfo<T>) => {
-      const key = keyExtractor(info.item, info.index);
-      const isActive = key === activeKey.current;
+      const key = keyExtractorRef.current(info.item, info.index);
+      const isActive = key === activeDataRef.current?.key;
       const onDragStart = () => {
         // We don't allow dragging for lists less than 2 elements
         if (data.length > 1) {
-          activeIndex.current = info.index;
-          activeKey.current = key;
-          panIndex.current = activeIndex.current;
+          activeDataRef.current = { index: info.index, key: key };
+          panIndex.current = info.index;
           setExtra({ activeKey: key, panIndex: info.index });
         }
       };
@@ -376,7 +400,7 @@ function DragListImpl<T>(
         // decided to call onStartDrag is likely in response to an onPressIn,
         // which then triggers on onPressOut the moment we capture (thus
         // leading to a premature call to onEndDrag here).
-        if (activeKey.current !== null && !panGrantedRef.current) {
+        if (activeDataRef.current && !panGrantedRef.current) {
           reset();
         }
       };
@@ -423,12 +447,11 @@ function DragListImpl<T>(
 
   return (
     <DragListProvider
-      activeKey={activeKey.current}
-      activeIndex={activeIndex.current}
-      keyExtractor={keyExtractor}
+      activeData={activeDataRef.current}
+      keyExtractor={keyExtractorRef.current}
       pan={pan}
       panIndex={panIndex.current}
-      panOpacity={panOpacity}
+      isReordering={isReorderingRef.current}
       layouts={layouts}
       horizontal={props.horizontal}
     >
@@ -449,12 +472,12 @@ function DragListImpl<T>(
               }
             }
           }}
-          keyExtractor={keyExtractor}
-          data={data}
+          keyExtractor={keyExtractorRef.current}
+          data={dataRef.current}
           renderItem={renderDragItem}
           CellRendererComponent={CellRendererComponent}
           extraData={extra}
-          scrollEnabled={!activeKey.current}
+          scrollEnabled={!activeDataRef.current}
           onScroll={onDragScroll}
           scrollEventThrottle={16} // From react-native-draggable-flatlist; no idea why.
           removeClippedSubviews={false} // https://github.com/facebook/react-native/issues/18616
@@ -483,16 +506,15 @@ function CellRendererComponent<T>(props: CellRendererProps<T>) {
   const { item, index, children, onLayout, ...rest } = props;
   const {
     keyExtractor,
-    activeKey,
-    activeIndex,
+    activeData,
     pan,
     panIndex,
-    panOpacity,
+    isReordering,
     layouts,
     horizontal,
   } = useDragListContext<T>();
   const key = keyExtractor(item, index);
-  const isActive = key === activeKey;
+  const isActive = key === activeData?.key;
   const anim = useRef(new Animated.Value(0)).current;
   // https://github.com/fivecar/react-native-draglist/issues/53
   // Starting RN 0.76.3, we need to use Animated.Value instead of a plain number
@@ -506,7 +528,6 @@ function CellRendererComponent<T>(props: CellRendererProps<T>) {
             elevation: ANIM_VALUE_ONE,
             zIndex: ANIM_VALUE_NINER,
             transform: [horizontal ? { translateX: pan } : { translateY: pan }],
-            opacity: panOpacity,
           }
         : {
             elevation: ANIM_VALUE_ZERO,
@@ -516,7 +537,7 @@ function CellRendererComponent<T>(props: CellRendererProps<T>) {
             ],
           },
     ];
-  }, [props.style, isActive, horizontal, pan, panOpacity, anim]);
+  }, [props.style, isActive, horizontal, pan, anim]);
   const onCellLayout = useCallback(
     (evt: LayoutChangeEvent) => {
       if (onLayout) {
@@ -534,33 +555,45 @@ function CellRendererComponent<T>(props: CellRendererProps<T>) {
   // away, even on this very render (e.g. cases where we set it immediately to zero), whereas an
   // effect would render this without that change first, and then start changing anim.
   const _animCharge = useMemo(() => {
-    if (activeKey && !isActive && layouts.hasOwnProperty(activeKey)) {
-      if (index >= panIndex && index <= activeIndex) {
-        return Animated.timing(anim, {
-          duration: SLIDE_MILLIS,
-          easing: Easing.inOut(Easing.linear),
-          toValue: layouts[activeKey].extent,
-          useNativeDriver: true,
-        }).start();
-      } else if (index >= activeIndex && index <= panIndex) {
-        return Animated.timing(anim, {
-          duration: SLIDE_MILLIS,
-          easing: Easing.inOut(Easing.linear),
-          toValue: -layouts[activeKey].extent,
-          useNativeDriver: true,
-        }).start();
+    if (isReordering) {
+      // Do not change anim when reordering. Even though it seems safe to do, iOS v. Android
+      // could/do recycle views and changing the anim will cause things to visually jump even if you
+      // think your rendering code shouldn't have that problem.
+      return;
+    }
+
+    if (activeData != null) {
+      const activeKey = activeData.key;
+      const activeIndex = activeData.index;
+
+      if (!isActive && layouts.hasOwnProperty(activeKey)) {
+        if (index >= panIndex && index <= activeIndex) {
+          return Animated.timing(anim, {
+            duration: SLIDE_MILLIS,
+            easing: Easing.inOut(Easing.linear),
+            toValue: layouts[activeKey].extent,
+            useNativeDriver: true,
+          }).start();
+        } else if (index >= activeIndex && index <= panIndex) {
+          return Animated.timing(anim, {
+            duration: SLIDE_MILLIS,
+            easing: Easing.inOut(Easing.linear),
+            toValue: -layouts[activeKey].extent,
+            useNativeDriver: true,
+          }).start();
+        }
       }
     }
     return Animated.timing(anim, {
-      duration: activeKey ? SLIDE_MILLIS : 0,
+      duration: activeData?.key ? SLIDE_MILLIS : 0,
       easing: Easing.inOut(Easing.linear),
       toValue: 0,
       useNativeDriver: true,
     }).start();
-  }, [activeKey, index, panIndex, key, activeIndex, horizontal]);
+  }, [index, panIndex, key, activeData, horizontal, isReordering]);
 
   return (
-    <Animated.View key={key} {...rest} style={style} onLayout={onCellLayout}>
+    <Animated.View {...rest} style={style} onLayout={onCellLayout}>
       {children}
     </Animated.View>
   );

src/DragListContext.tsx
@@ -15,15 +15,19 @@ export interface LayoutCache {
   [key: string]: PosExtent;
 }
 
+export interface ActiveData {
+  key: string;
+  index: number;
+}
+
 // This all basically enables us to pass data into a CellRendererComponent,
 // which we otherwise don't control the props to.
 type ContextProps<T> = {
-  activeKey: string | null;
-  activeIndex: number;
+  activeData: ActiveData | null;
   keyExtractor: (item: T, index: number) => string;
   pan: Animated.Value;
   panIndex: number;
-  panOpacity: Animated.Value;
+  isReordering: boolean;
   layouts: LayoutCache;
   horizontal: boolean | null | undefined;
   children: React.ReactNode;
@@ -36,37 +40,26 @@ const DragListContext = React.createContext<
 >(undefined);
 
 export function DragListProvider<T>({
-  activeKey,
-  activeIndex,
+  activeData,
   keyExtractor,
   pan,
   panIndex,
-  panOpacity,
+  isReordering,
   layouts,
   horizontal,
   children,
 }: ContextProps<T>) {
   const value = useMemo(
     () => ({
-      activeKey,
-      activeIndex,
+      activeData,
       keyExtractor,
       pan,
       panIndex,
-      panOpacity,
+      isReordering,
       layouts,
       horizontal,
     }),
-    [
-      activeKey,
-      activeIndex,
-      keyExtractor,
-      pan,
-      panIndex,
-      panOpacity,
-      layouts,
-      horizontal,
-    ]
+    [activeData, keyExtractor, pan, panIndex, isReordering, layouts, horizontal]
   );
 
   return (

commit 7fb999256fb4050ee8389e351b1c84a10cdd002b

fix: #95 Re-render on iOS after reordering
This fixes a regression from #85 which introduced badly-rendered lists
in iOS on subsequent drags. #85, funny enough, was trying to fix #76.

The core cause of this was that `inReorderingRef` was inconsistent
between renders right after reordering, triggered by `reset()`. We now
update the ref prior to the reset. Issue would only reproduce on device,
and only in more complicated apps than `/example` (I suspect because of
re-rendering speeds).

Also figured out we don't need to change keys on the items as long as we
made this change. All in all, seems to work better.

Tested on iOS device by integrating into a more complicated app, and on
Android simulator.

src/index.tsx
@@ -128,6 +128,8 @@ function DragListImpl<T>(
   );
   const reorderRef = useRef(props.onReordered);
   reorderRef.current = useMemo(() => props.onReordered, [props.onReordered]);
+  const keyExtractorRef = useRef(keyExtractor);
+  keyExtractorRef.current = useMemo(() => keyExtractor, [keyExtractor]);
 
   // #76 When we finalize a reordering (i.e. when our parent gets `onReordered`), we need to
   // insulate ourselves from the parent changing the data we render without us controlling the
@@ -159,20 +161,6 @@ function DragListImpl<T>(
     },
     [pan]
   );
-
-  // #76 - Add indirection to keyExtractor to change keys during reordering. Because native
-  // implementations can/do recycle views at times, you need to deliberately rekey things during
-  // reordering so the native implementation respects your new positions/styles/settings.
-  const keyExtractorRef = useRef(keyExtractor);
-  keyExtractorRef.current = useMemo(() => {
-    return (item: T, index: number) => {
-      const key = keyExtractor(item, index);
-      if (isReorderingRef.current) {
-        return key + "_reordering";
-      }
-      return key;
-    };
-  }, [keyExtractor]);
 
   const shouldCapturePan = useCallback(() => {
     return !!activeDataRef.current && !isReorderingRef.current;
@@ -341,8 +329,10 @@ function DragListImpl<T>(
 
           await reorderRef.current?.(activeIndex, panIndex.current);
         } finally {
-          reset(); // Guarantee resetting by putting this in finally
+          // This needs to come before reset(), which causes a re-render that depends on
+          // isReorderingRef.current reflecting the fact we're not reordering anymore.
           isReorderingRef.current = false;
+          reset(); // Guarantee resetting by putting this in finally
         }
       } else {
         // #76 - Only reset here if we're not going to reorder the list. If we are instead